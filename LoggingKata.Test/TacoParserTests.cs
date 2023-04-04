ec85c514801db132e4bb137cddbef9a5bbff4f12
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-04-04 16:38:12 -0500

    TacoParse Project

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -23,6 +23,10 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
+        [InlineData("33.22997, -86.805275, Taco Bell Alabaste...", -86.805275)]
+        [InlineData("33.635282,-86.684056,Taco Bell Birmingham...", -86.684056)]
+        [InlineData("32.571331,-85.499655,Taco Bell Auburn...",-85.499655)]
+
         public void ShouldParseLongitude(string line, double expected)
         {
             // TODO: Complete - "line" represents input data we will Parse to
@@ -30,14 +34,35 @@ namespace LoggingKata.Test
             //       each representing a TacoBell location
 
             //Arrange
-
+            var parser = new TacoParser();
             //Act
+            var actual = parser.Parse(line);
 
             //Assert
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
         //TODO: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
+        [InlineData("33.587217,-85.057114,Taco Bell Carrollto...", -85.057114)]
+        [InlineData("34.196869,-84.135598,Taco Bell Cumming...", -84.135598)]
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            //Arrange
+            var parser = new TacoParser();
+
+            //Act
+            var actual = parser.Parse(line);
+
+            //Assert
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
+
+
+
+
 
     }
 }
